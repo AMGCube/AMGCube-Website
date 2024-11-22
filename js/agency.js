1b40ada4d5803e381e462ea3d0baaa77ab5d3808
AMGcube <leon.yq@gmail.com>
2024-11-22 18:02:35 +1000

    Update agency.js
    修复portfolio模块跳转无法回到首页的错误

--- js/agency.js
+++ js/agency.js
@@ -1,11 +1,11 @@
 /*!
- * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
+$(document).ready(function() {
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
@@ -13,25 +13,37 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-});
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top'
+    });
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
 
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
+    // Handle the modal hash in URL when a modal is opened
+    $('div.modal').on('show.bs.modal', function() {
+        var modal = this;
+        var hash = modal.id;
+        window.location.hash = hash;
+        window.onhashchange = function() {
+            if (!location.hash) {
+                $(modal).modal('hide');
+            }
+        };
+    });
+
+    // Clear the URL hash when the modal is closed
+    $('div.modal').on('hidden.bs.modal', function() {
+        history.pushState("", document.title, window.location.pathname + window.location.search);
+        console.log('Modal closed and hash cleared');
+
+        // Move focus to a different element to prevent aria-hidden issues
+        setTimeout(function() {
+            $('.navbar-brand').focus();  // Assuming moving focus to the navbar brand link
+        }, 500);
+    });
 });
